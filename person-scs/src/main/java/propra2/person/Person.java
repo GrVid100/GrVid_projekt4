755ae002565e02cbf7b85068452c9e1e36f10e95
nguyenthanhtungn5 <nguyenthanhtungn5@gmail.com>
2019-01-06 14:20:56 +0100

    name verändern

--- person-scs/src/main/java/propra2/person/Person.java
+++ person-scs/src/main/java/propra2/person/Person.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.lang.reflect.Array;
 import java.util.List;
 
 @Data
@@ -13,57 +14,10 @@ public class Person {
     @Id
     @GeneratedValue
     private Long id;
-    private String name;
-    private String jahresLohn;
-    private String kontaktDaten;
-    private String skills;
-    //private List<Long> projektId;
-    //
-    //public List<Long> getProjektId() {
-    //    return projektId;
-    //}
-    //
-    //public void setProjektId(List<Long> projektId) {
-    //    this.projektId = projektId;
-    //}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getJahresLohn() {
-        return jahresLohn;
-    }
-
-    public String getKontaktDaten() {
-        return kontaktDaten;
-    }
-
-    public String getSkills() {
-        return skills;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setJahresLohn(String jahresLohn) {
-        this.jahresLohn = jahresLohn;
-    }
-
-    public void setKontaktDaten(String kontaktDaten) {
-        this.kontaktDaten = kontaktDaten;
-    }
-
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
+    private String vorname;
+    private String nachname;
+    private String rolle;
+    private String kontakt;
+    private Array<String> skills;
+    private Array<Long> projekte;
 }
